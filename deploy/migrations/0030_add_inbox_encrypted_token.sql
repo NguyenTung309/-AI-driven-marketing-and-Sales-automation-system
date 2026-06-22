873d0484b7ba41e1e89eeaec3f524278395c5046
Thanglm240723 <thanglm240723@gmail.com>
2026-06-22 23:57:52 +0700

    feat: add EncryptedAccessToken to Inbox entity

---
+++ deploy/migrations/0030_add_inbox_encrypted_token.sql
@@ -0,0 +1,3 @@
+﻿-- 0030_add_inbox_encrypted_token.sql
+-- Add EncryptedAccessToken column to Inboxes table for storing Pancake page_access_token
+ALTER TABLE Inboxes ADD EncryptedAccessToken NVARCHAR(1024) NULL;
